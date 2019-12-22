5c785e5fa1a391297955305a9c90e12df8d424da
Murali Rajagopalan <jana.rajagopalan@gmail.com>
2019-12-22 10:50:12 -0800

    add badminton

--- components/schedule.js
+++ components/schedule.js
@@ -105,7 +105,7 @@ export default class Schedule extends React.Component {
 										>
 											<div className="item-text">
 												<h4>Sunday</h4>
-												<h5>5 Jan</h5>
+												<h5>26 Jan</h5>
 											</div>
 										</a>
 									</li>
@@ -150,6 +150,78 @@ export default class Schedule extends React.Component {
 												<div className="card-body">
 													<div className="location">
 														< a href="/volleyball-2019">Registration and More details</a> <br />
+														<span>Location:</span> TBA
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="schedule-area row wow fadeInDown" data-wow-delay="0.3s">
+						<div className="schedule-tab-title col-md-3 col-lg-3 col-xs-12">
+							<div className="table-responsive">
+								<ul className="nav nav-tabs" id="myTab" role="tablist">
+									<li className="nav-item">
+										<a
+											className="nav-link active"
+											id="monday-tab"
+											data-toggle="tab"
+											href="#monday"
+											role="tab"
+											aria-controls="monday"
+											aria-expanded="true"
+										>
+											<div className="item-text">
+												<h4>Sunday</h4>
+												<h5>26 Jan</h5>
+											</div>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div className="schedule-tab-content col-md-9 col-lg-9 col-xs-12 clearfix">
+							<div className="tab-content" id="myTabContent">
+								<div
+									className="tab-pane fade show active"
+									id="monday"
+									role="tabpanel"
+									aria-labelledby="monday-tab"
+								>
+									<div id="accordion">
+										<div className="card">
+											<div id="headingOne">
+												<div
+													className="collapsed card-header"
+													data-toggle="collapse"
+													data-target="#collapseOne"
+													aria-expanded="false"
+													aria-controls="collapseOne"
+												>
+													<div className="images-box">
+														<img
+															className="img-fluid"
+															src="assets/img/speaker/speakers-1.png"
+															alt=""
+														/>
+													</div>
+													<span className="time">8am - 8pm</span>
+													<h4>Kids Badminton Tournament</h4>
+												</div>
+											</div>
+											<div
+												id="collapseOne"
+												className="collapse show"
+												aria-labelledby="headingOne"
+												data-parent="#accordion"
+											>
+												<div className="card-body">
+													<div className="location">
+														< a href="/badminton2020">Registration and More details</a> <br />
 														<span>Location:</span> TBA
 													</div>
 												</div>
